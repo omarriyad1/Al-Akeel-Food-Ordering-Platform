66128fdeaad486c69c7805508659b8765bbd31a9
shoroukanssary <119341093+shoroukanssary@users.noreply.github.com>
2023-05-21 19:16:50 +0300

    Add files via upload

---
+++ AlAkeelService.java
@@ -0,0 +1,12 @@
+package application.webservices;
+
+import javax.ejb.Stateless;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Stateless
+@Path("/n")
+@Produces(javax.ws.rs.core.MediaType.APPLICATION_JSON)
+public class AlAkeelService {
+
+}
